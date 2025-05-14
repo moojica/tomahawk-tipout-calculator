9bf513fa914a277bb95d00b0aa16559470f8a5f5
moojica <marcmujica@yahoo.com>
2025-05-14 01:39:36 -0400

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/tomahawk-tipout-calculator/sw.js', { scope: '/tomahawk-tipout-calculator/' })})}
